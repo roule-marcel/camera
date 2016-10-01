d696c3b28e46573ece05dc6d6e923a8b5f674ac9
jfellus <jfellus.gmail.com>
2016-10-01 20:35:07 +0200

    ok

--- src/server.js
+++ src/server.js
@@ -4,12 +4,6 @@ var A = null;
 var B = null;
 var wss = new WebSocketServer({port: 3434});
 
-wss.broadcast = function(data) {
-    for(var i in this.clients) {
-        this.clients[i].send(data);
-    }
-};
-
 wss.on('connection', function(ws) {
 	if(A===null) {
 		A = ws;
@@ -25,9 +19,4 @@ wss.on('connection', function(ws) {
 	        if(A) A.send(message);
 	    });
 	}
-
-    ws.on('message', function(message) {
-        console.log('received: %s', message);
-        wss.broadcast(message);
-    });
 });
